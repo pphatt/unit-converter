b6863ca9bb09e3258ff12a3b61e8c6f1aa30b993
pphatt <vutienphat0809@gmail.com>
2024-10-06 00:05:17 +0700

    feat: disable keyboard popup when focus on the text input

--- app/src/main/java/app/unitconverter/ui/components/common/NumberInputField.kt
+++ app/src/main/java/app/unitconverter/ui/components/common/NumberInputField.kt
@@ -6,33 +6,59 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.InterceptPlatformTextInput
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalTextInputService
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.awaitCancellation
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable fun DisableSoftKeyboard(
+    disable: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    InterceptPlatformTextInput(
+        interceptor = { request, nextHandler ->
+            if (!disable) {
+                nextHandler.startInputMethod(request)
+            } else {
+                awaitCancellation()
+            }
+        },
+        content = content,
+    )
+}
 
 @Composable
 fun NumberInputField(
     modifier: Modifier = Modifier, value: String
 ) {
-    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-        OutlinedTextField(
-            modifier = modifier.fillMaxWidth(),
-            value = value,
-            onValueChange = {},
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Transparent,
-                unfocusedBorderColor = Color.Transparent
-            ),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next,
-            ),
-            textStyle = TextStyle.Default.copy(fontSize = 30.sp)
-        )
+    CompositionLocalProvider(
+        LocalLayoutDirection provides LayoutDirection.Rtl,
+    ) {
+        DisableSoftKeyboard {
+            OutlinedTextField(
+                modifier = modifier
+                    .fillMaxWidth(),
+                value = value,
+                onValueChange = {},
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Default,
+                ),
+                textStyle = TextStyle.Default.copy(fontSize = 30.sp)
+            )
+        }
     }
 }
